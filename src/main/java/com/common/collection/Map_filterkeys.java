9371c25299879ce875a8202dd131d652fa669105
liusengen <1518759402@qq.com>
2019-01-28 09:36:02 +0800

    qing li

--- src/main/java/com/common/collection/Map_filterkeys.java
+++ src/main/java/com/common/collection/Map_filterkeys.java
@@ -4,32 +4,37 @@ import org.apache.hadoop.hive.ql.exec.UDFArgumentException;
 import org.apache.hadoop.hive.ql.metadata.HiveException;
 import org.apache.hadoop.hive.ql.udf.generic.GenericUDF;
 import org.apache.hadoop.hive.serde2.objectinspector.*;
+import org.apache.log4j.Logger;
 
-import java.util.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @program: myUdf
- * @description: 用于去掉map的某些key对应的键值对
+ * @description: 用于过滤出map里指定key的键值对
  * @author: LiuSenGen
- * @create: 2019-01-26 16:14
+ * @create: 2019-01-26 18:01
  **/
-public class Map_removekeys extends GenericUDF {
-    private MapObjectInspector mapInspector; //第一个参数的类型
-    private ListObjectInspector keyListInspector; //第二个参数的类型
-    private StandardMapObjectInspector retValInspector; //返回的参数类型
+public class Map_filterkeys extends GenericUDF {
+    private static final Logger LOG = Logger.getLogger(Map_filterkeys.class);
+    private MapObjectInspector mapInspector; //第一个参数类型
+    private ListObjectInspector keyListInspector;//第二个参数类型
+    private StandardMapObjectInspector retValInspector; //返回值类型
 
 
     //参数初始化
     @Override
     public ObjectInspector initialize(ObjectInspector[] arg0) throws UDFArgumentException {
-        //检查第一个参数是否为map
+        //检查第一个值是否为map
         ObjectInspector first = arg0[0];
         if (first.getCategory() == ObjectInspector.Category.MAP) {
             mapInspector = (MapObjectInspector) first;
         } else {
             throw new UDFArgumentException(" Expecting a map as first argument ");
         }
-       //检查第二个参数是否为list
+        //检查第二个值是否为list
         ObjectInspector second = arg0[1];
         if (second.getCategory() == ObjectInspector.Category.LIST) {
             keyListInspector = (ListObjectInspector) second;
@@ -37,6 +42,7 @@ public class Map_removekeys extends GenericUDF {
             throw new UDFArgumentException(" Expecting a list as second argument ");
         }
 
+        //检查list的值类型和map的key的类型是否一致
         if (!(keyListInspector.getListElementObjectInspector().getCategory() == ObjectInspector.Category.PRIMITIVE)) {
             throw new UDFArgumentException(" Expecting a primitive as key list elements.");
         }
@@ -44,40 +50,14 @@ public class Map_removekeys extends GenericUDF {
         if (!(mapKeyInspector.getCategory() == ObjectInspector.Category.PRIMITIVE)) {
             throw new UDFArgumentException(" Expecting a primitive as map key elements.");
         }
-
-        //检查map的key和list的值是否为同一类型
+        LOG.info(" Map has key type " + mapKeyInspector.getTypeName());
+        LOG.info(" Key list has key type " + keyListInspector.getTypeName());
         if (((PrimitiveObjectInspector) keyListInspector.getListElementObjectInspector()).getPrimitiveCategory() != ((PrimitiveObjectInspector) mapKeyInspector).getPrimitiveCategory()) {
             throw new UDFArgumentException(" Expecting keys to be of same types.");
         }
 
         retValInspector = (StandardMapObjectInspector) ObjectInspectorUtils.getStandardObjectInspector(first);
         return retValInspector;
-    }
-
-    @Override
-    public Object evaluate(DeferredObject[] arg0) throws HiveException {
-        Map hiveMap = mapInspector.getMap(arg0[0].get());
-
-        List keyValues = inspectList(keyListInspector.getList(arg0[1].get()));
-
-        Map stdKeys = stdKeys(hiveMap);
-
-        Set stdKeySet = stdKeys.keySet();
-        List stdKeyList = new ArrayList(stdKeySet);
-
-        Map retVal = (Map) retValInspector.create();
-        TreeMap retValSorted = new TreeMap(retVal);
-        for (Object keyObj : stdKeyList) {
-            if (!keyValues.contains(keyObj)) {
-                Object hiveKey = stdKeys.get(keyObj);
-                Object hiveVal = hiveMap.get(hiveKey);
-                Object keyStd = ObjectInspectorUtils.copyToStandardObject(hiveKey, mapInspector.getMapKeyObjectInspector());
-                Object valStd = ObjectInspectorUtils.copyToStandardObject(hiveVal, mapInspector.getMapValueObjectInspector());
-
-                retValSorted.put(keyStd, valStd);
-            }
-        }
-        return retValSorted;
     }
 
     private Map stdKeys(Map inspectMap) {
@@ -105,7 +85,27 @@ public class Map_removekeys extends GenericUDF {
     }
 
 
+    @Override
+    public Object evaluate(DeferredObject[] arg0) throws HiveException {
+        Map hiveMap = mapInspector.getMap(arg0[0].get());
 
+        List keyValues = inspectList(keyListInspector.getList(arg0[1].get()));
+
+        Map stdKeys = stdKeys(hiveMap);
+
+        Map retVal = (Map) retValInspector.create();
+        for (Object keyObj : keyValues) {
+            if (stdKeys.containsKey(keyObj)) {
+                Object hiveKey = stdKeys.get(keyObj);
+                Object hiveVal = hiveMap.get(hiveKey);
+                Object keyStd = ObjectInspectorUtils.copyToStandardObject(hiveKey, mapInspector.getMapKeyObjectInspector());
+                Object valStd = ObjectInspectorUtils.copyToStandardObject(hiveVal, mapInspector.getMapValueObjectInspector());
+
+                retVal.put(keyStd, valStd);
+            }
+        }
+        return retVal;
+    }
 
 
     @Override
